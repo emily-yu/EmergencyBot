c10fcd3880de0ec5c1f024eec21beea708396e21
ey1031 <eyudeveloper@gmail.com>
2017-02-18 20:37:34 -0800

    twilio sms working

---
+++ emergency app thing try 2/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from twilio.rest import TwilioRestClient
+ 
+app = Flask(__name__)
+ 
+# put your own credentials here 
+ACCOUNT_SID = 'SID' 
+AUTH_TOKEN = 'TOKEN' 
+ 
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+ 
+@app.route('/sms', methods=['POST'])
+def send_sms():
+    message = client.messages.create(
+        to=request.form['To'], 
+        from_='PHONE#', 
+        body=request.form['Body'],
+    )
+ 
+    return message.sid
+ 
+if __name__ == '__main__':
+        app.run()
+
