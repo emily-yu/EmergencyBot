7850b1260664640bfc28b016528f82c2649d4ce8
ey1031 <eyudeveloper@gmail.com>
2017-03-01 12:58:12 -0800

    asdf

---
+++ EmergencyApp (Twilio)/TwilioSms/TwilioSms-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Bridging.m
+//  
+//
+//  Created by Emily on 3/1/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <APAddressBook/APAddressBook-Bridging.h>
+
---
+++ EmergencyApp (Twilio)/TwilioSms/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  TwilioSms
+//
+//  Created by Emily on 2/8/17.
+//  Copyright © 2017 Emily. All rights reserved.
+// To run: python app.py in one terminal shell, then ./ngrok http 5000 in another and then run xcode
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var phoneNumberField: UITextField!
+    @IBOutlet var messageField: UITextField!
+    
+    @IBAction func sendData(sender: AnyObject) {
+        let headers = [
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
+        
+        let parameters: Parameters = [
+            "To": phoneNumberField.text ?? "",
+            "Body": messageField.text ?? ""
+        ]
+        
+        Alamofire.request("http://f4bde39d.ngrok.io/sms", method: .post, parameters: parameters, headers: headers).response { response in
+            print(response)
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
