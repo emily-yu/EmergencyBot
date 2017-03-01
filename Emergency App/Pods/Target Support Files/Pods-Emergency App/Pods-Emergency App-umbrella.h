b60da833d73226e1debb7538d9e60647d1128ec7
ey1031 <eyudeveloper@gmail.com>
2017-03-01 13:02:07 -0800

    get rid of gunnhacks fb stuff

--- Emergency App/Pods/Target Support Files/Pods-Emergency App/Pods-Emergency App-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Emergency_AppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Emergency_AppVersionString[];
-
--- Emergency App/Pods/Target Support Files/Pods-Emergency AppTests/Pods-Emergency AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Emergency_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_Emergency_AppTests
-@end
