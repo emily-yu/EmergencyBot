39136392e3927af8e734197b6f1c092115ad8d76
ey1031 <eyudeveloper@gmail.com>
2017-06-05 01:03:52 -0700

    firebase customizecontact

--- TwilioSms/CustomizeContact.swift
+++ TwilioSms/CustomizeContact.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import Firebase
 
 class CustomizeContactController: UIViewController {
     
@@ -17,6 +18,8 @@ class CustomizeContactController: UIViewController {
     @IBOutlet var stepper: UIStepper!
     @IBOutlet var deleteContact: UIButton!
     @IBOutlet var delayLabel: UILabel!
+    
+    var ref: FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,36 +43,49 @@ class CustomizeContactController: UIViewController {
     
     @IBAction func done(_ sender: Any) {
         // Saved Locally
+        ref = FIRDatabase.database().reference()
+        let contactPath = self.ref.child(FIRAuth.auth()!.currentUser!.uid).child("contacts").child(String(clickedIndex))
+        
         if emergencyIndexes.contains("\(clickedIndex)") { // is an emergency contact
-            var eIndex = emergencyIndexes.index(of:"\(clickedIndex)")
+            let eIndex = emergencyIndexes.index(of:"\(clickedIndex)")
             if (name.text != nil) {
                 emergencyNames[eIndex!] = name.text!
                 contactNames[clickedIndex] = name.text!
+                contactPath.child("name").setValue(name.text!)
             }
             else if (phoneNumber.text != nil) {
                 emergencyNumbers[eIndex!] = Int(phoneNumber.text!)!
+                self.ref.child(FIRAuth.auth()!.currentUser!.uid).child("emergencyContact").child(String(describing: eIndex)).setValue(Int(phoneNumber.text!))
+                
                 contactNumbers[clickedIndex] = Int(phoneNumber.text!)!
+                contactPath.child("number").setValue(Int(phoneNumber.text!))
             }
             else if (Float(stepper.value) != emergencyDelay[eIndex!]) {
                 emergencyDelay[eIndex!] = Float(stepper.value)
                 contactDelay[clickedIndex] = Float(stepper.value)
+                contactPath.child("delay").setValue(Float(stepper.value))
             }
             else if (additionalNotes.text != contactAdditionalInfo[clickedIndex]) {
                 contactAdditionalInfo[clickedIndex] = additionalNotes.text
+                contactPath.child("notes").setValue(additionalNotes.text)
             }
         }
         else {
             if (name.text != nil) {
                 contactNames[clickedIndex] = name.text!
+                contactPath.child("name").setValue(name.text!)
             }
             else if (phoneNumber.text != nil) {
                 contactNumbers[clickedIndex] = Int(phoneNumber.text!)!
+                contactPath.child("number").setValue(Int(phoneNumber.text!))
             }
             else if (Float(stepper.value) != contactDelay[clickedIndex]) {
                 contactDelay[clickedIndex] = Float(stepper.value)
+                contactPath.child("delay").setValue(Float(stepper.value))
             }
             else if (additionalNotes.text != contactAdditionalInfo[clickedIndex]) {
                 contactAdditionalInfo[clickedIndex] = additionalNotes.text
+                contactPath.child("notes").setValue(additionalNotes.text)
             }
         }
     }
@@ -80,7 +96,6 @@ class CustomizeContactController: UIViewController {
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "contactSegue"){
-            
             if let tabVC = segue.destination as? UITabBarController{
                 tabVC.selectedIndex = 0
                 tabVC.modalPresentationStyle = .custom
